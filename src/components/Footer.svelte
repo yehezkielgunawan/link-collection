b59c4d405f3b258fa3ad80a13e4b0a7092d517b6
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2024-05-09 10:31:56 +0700

    feat(Footer): :sparkles: add footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,7 @@
+<footer class="mt-8">
+  <a class="link link-hover" href="https://yehezgun.com"
+    >{new Date().toLocaleDateString("en-GB", {
+      year: "numeric",
+    })} | Yehezkiel Gunawan</a
+  >
+</footer>
