0e7f58d584ef82da08c0165b753d7d78292ab787
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2024-05-24 10:26:51 +0700

    fix(Headers.svelte): :bug: add github repo nav

--- src/components/Headers.svelte
+++ src/components/Headers.svelte
@@ -1,4 +1,6 @@
 <script>
+  import Icon from "@iconify/svelte";
+
   export let title = "YehezGun's Links";
 </script>
 
@@ -12,5 +14,14 @@
       <img src="/icons8-link-48.png" alt="icon" width="24" height="24" />
       <a href="/" class="text-lg font-bold">YehezGun's Links Collection</a>
     </div>
+  </div>
+  <div class="flex-none">
+    <a
+      href="https://github.com/yehezkielgunawan/link-collection"
+      class="btn btn-ghost"
+      target="_blank"
+    >
+      <Icon icon={`bxl:github`} width={24} height={24} /> Github Repo</a
+    >
   </div>
 </nav>
